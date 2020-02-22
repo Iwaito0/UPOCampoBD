35c46f0ded112fbb8879553e74201459288e3e9d
Salvador-B <59707016+Salvador-B@users.noreply.github.com>
2020-02-22 20:20:39 +0100

    Create modificarProveedores.php

---
+++ Proveedor/modificarProveedores.php
@@ -0,0 +1,28 @@
+<?php
+	// Configuración BASE DE DATOS MYSQL
+	$servidor  = "localhost";
+	$basedatos = "upocampo";
+	$usuario   = "root";
+	$password  = "";
+
+	// Creamos la conexión al servidor.
+	$conexion = mysqli_connect($servidor, $usuario, $password,$basedatos) or die(enviarResultados(0,[],$conexion));
+	mysqli_set_charset($conexion,"utf8");
+	$datos=json_decode($_POST["datos"]);
+
+	$sql = "UPDATE proveedor SET nombre='$datos->Nombre', telefono='$datos->Telefono' WHERE CIF='$datos->CIF'";
+
+	$resultados = mysqli_query($conexion,$sql);
+
+	if ($resultados){
+	    $respuesta["error"] = 0;
+	    $respuesta["mensaje"] = "Modificacion realizada"; 
+	} else {
+	    $respuesta["error"] = 1;
+	    $respuesta["mensaje"] = "Error en el proceso de modificacion: ".mysqli_error($conexion);
+	}
+
+	echo json_encode($respuesta);
+
+	mysqli_close($conexion);
+?>
