8b2a0b7555af54192ddbc7b5d7b2a4892d146a65
Salvador-B <59707016+Salvador-B@users.noreply.github.com>
2020-02-23 13:43:35 +0100

    Create listadoHabitaciones.js

---
+++ Habitaciones/js/listadoHabitaciones.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let pestana=window.open();
+$.get("Habitaciones/getHabitaciones.php",rellenarCampos,'json');
+
+function rellenarCampos(oDatos, sStatus, oXHR) {
+	//console.log(JSON.stringify(oDatos));
+	pestana.document.write("<html><head><title>Listado General - Habitaciones</title><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'></head></html>");
+	//Creacion de la tabla
+    let oTabla=document.createElement("TABLE");
+    oTabla.setAttribute("border","1");
+    oTabla.classList.add("table","table-striped","table-dark");
+    //El encabezado de la tabla
+    let oTHead=oTabla.createTHead();
+    let oFila=oTHead.insertRow(-1);
+    let oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Numero de habitacion";
+
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Tipo";
+
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Precio";
+
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Ocupacion Maxima";
+
+    //El cuerpo de la tabla
+    let oTBody = document.createElement("TBODY");
+    oTabla.appendChild(oTBody);
+
+    for(let i=0; i<oDatos["datos"].length; i++){
+        let oFila = oTBody.insertRow(-1);
+
+        let oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["id"];
+
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["tipo"];
+
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["precio"];
+
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["ocupacion_max"];
+    }
+
+    pestana.document.body.append(oTabla);
+}
