c164793421c174f11bad1e0b8897c9b28ce9aead
Javier Marín <javiermarindz@gmail.com>
2020-02-23 17:06:31 +0100

    Corrección declaración pestana
    La nueva pestaña no se puede crear más de una vez, solucionado.

--- Habitaciones/js/listadoHabitaciones.js
+++ Habitaciones/js/listadoHabitaciones.js
@@ -1,9 +1,10 @@
 "use strict";
 
-let pestana=window.open();
+
 $.get("Habitaciones/getHabitaciones.php",rellenarCampos,'json');
 
 function rellenarCampos(oDatos, sStatus, oXHR) {
+    let pestana=window.open();
 	//console.log(JSON.stringify(oDatos));
 	pestana.document.write("<html><head><title>Listado General - Habitaciones</title><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'></head></html>");
 	//Creacion de la tabla
