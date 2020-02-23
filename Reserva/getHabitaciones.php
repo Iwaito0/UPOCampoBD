51045c5d4833348eec822b6472ad9452938ddd8e
Salvador-B <59707016+Salvador-B@users.noreply.github.com>
2020-02-23 22:21:12 +0100

    Update getHabitaciones.php

--- Reserva/getHabitaciones.php
+++ Reserva/getHabitaciones.php
@@ -23,18 +23,34 @@
 	    }
 	}
 
+	// Consulta SQL para obtener los datos de los centros.
+	$sql2 = "SELECT * FROM reserva";
+	$resultados2 = mysqli_query($conexion,$sql2) or die(mysqli_error($conexion));
+
+	if ($resultados2){ // Si hay resultados
+
+	    $reservas = [];
+
+	    while ($fila = mysqli_fetch_array($resultados2)) {
+	       // Almacenamos en un array cada una de las filas que vamos leyendo del recordset.
+	        $reservas[] = $fila;
+	    }
+	}
+
 	mysqli_close($conexion);
 
-	enviarResultados($resultados,$datos,$conexion);
+	enviarResultados($resultados,$datos,$reservas,$conexion);
 
-	function enviarResultados($resultados,$datos,$conexion){
+	function enviarResultados($resultados,$datos,$reservas,$conexion){
 	    // Generar la respuesta
 	    header('Content-Type: application/json');
 
 	    if ($resultados ){ 
 	        $respuesta["datos"] = $datos;
+	        $respuesta["reservas"] = $reservas;
 	    } else {
 	        $respuesta["datos"] = [];
+	        $respuesta["reservas"] = [];
 	    }
 
 	    echo json_encode($respuesta);
