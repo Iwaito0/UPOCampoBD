47ffcfd432e5c5c9e2ba58165f95b85e8ba839be
Iwaito0 <57145421+Iwaito0@users.noreply.github.com>
2020-02-24 11:09:03 +0100

    lisatdoclientefecga

--- Clientes/js/listadoClienteFecha.js
+++ Clientes/js/listadoClienteFecha.js
@@ -4,7 +4,70 @@ $("#btnListarCliRes").click(aceptarListadoClienteFecha);
 function aceptarListadoClienteFecha(){
     let datosEntrada=frmListadoCliResFecha.txtEntradaCliRes.value;
     let datosSalida=frmListadoCliResFecha.txtSalidaCliRes.value;
-    console.log(datosEntrada);
-    console.log(datosSalida);
-    //SELECT * FROM `cliente` INNER JOIN reserva WHERE dni=nif_cliente and checkin=date("2020-02-12") and checkout=date("2020-02-16") 
+        var oFechas = {
+          fechaEntrada: datosEntrada,
+          fechaSalida:  datosSalida
+        };
+        var sParametros = "datos=" + JSON.stringify(oFechas);
+        $.get("Clientes/listadoClientesFechas.php", sParametros, respuestaClientesFecha, 'json');
+
+
 }
+function respuestaClientesFecha(oDatos, sStatus, oXHR){
+    //console.log(oDatos["datos"]);
+    let pestana=window.open();
+	//console.log(JSON.stringify(oDatos));
+	pestana.document.write("<html><head><title>Listado General - Clientes</title><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'></head></html>");
+	let oTabla=document.createElement("TABLE");
+    oTabla.setAttribute("border","1");
+    oTabla.classList.add("table","table-striped","table-dark");
+    //El encabezado de la tablais
+    let oTHead=oTabla.createTHead();
+    let oFila=oTHead.insertRow(-1);
+    let oCelda=oFila.insertCell(-1);
+    oCelda.textContent="NIF";
+    
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Nombre";
+    
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Telefono";
+    
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Direccion";
+
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Email";
+
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Numero de Tarjeta";
+    //El cuerpo de la tabla
+    let oTBody = document.createElement("TBODY");
+    oTabla.appendChild(oTBody);
+    
+    for(let i=0; i<oDatos["datos"].length; i++){
+        let oFila = oTBody.insertRow(-1);
+    
+        let oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["dni"];
+    
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["nombre"];
+
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["telefono"];
+
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["direccion"];
+
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["email"];
+
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = oDatos["datos"][i]["numero_tarjeta"];
+        
+    }
+    
+    pestana.document.body.append(oTabla);
+}
+    
