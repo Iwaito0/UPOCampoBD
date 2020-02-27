1548b3c735bb78977e2f71e0d923415c1236d78a
Salvador-B <59707016+Salvador-B@users.noreply.github.com>
2020-02-27 16:35:12 +0100

    Create bajaReserva.php

---
+++ Reserva/bajaReserva.php
@@ -0,0 +1,26 @@
+<?php
+	// Configuración BASE DE DATOS MYSQL
+	$servidor  = "localhost";
+	$basedatos = "upocampo";
+	$usuario   = "root";
+	$password  = "";
+
+	// Creamos la conexión al servidor.
+	$conexion = mysqli_connect($servidor, $usuario, $password,$basedatos) or die(mysqli_error($conexion));
+	mysqli_query($conexion,"utf8");
+
+	$sql='DELETE FROM reserva WHERE id='.$_POST["datos"];
+	$resultados = mysqli_query($conexion,$sql) or die(mysqli_error($conexion));
+
+	if ($resultados){
+	    $respuesta["error"] = 0;
+	    $respuesta["mensaje"] = "Baja realizada"; 
+	} else {
+	    $respuesta["error"] = 1;
+	    $respuesta["mensaje"] = "Error en el proceso de baja: ".mysqli_error($conexion);
+	}
+
+	echo json_encode($respuesta);
+
+	mysqli_close($conexion);
+?>
